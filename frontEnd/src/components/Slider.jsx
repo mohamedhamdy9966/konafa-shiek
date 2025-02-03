9178aa4d1c5023a7550ea1c856d391aa6cb6ff47
mohamedhamdy9966 <mh7008002@gmail.com>
2025-02-03 14:34:15 +0200

    add slider in home page

--- frontEnd/src/components/Slider.jsx
+++ frontEnd/src/components/Slider.jsx
@@ -1,33 +1,68 @@
 import React from "react";
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import { assets } from "../assets/assets";
 
 const Slider = () => {
-  const carouselOptions = {
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive: {
-      0: { items: 1 },
-      600: { items: 2 },
-      1000: { items: 3 },
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1,
     },
   };
   return (
     <div className="container mx-auto px-4 py-8">
-      <OwlCarousel className="owl-theme" {...carouselOptions}>
-        <div className="item">
-          <img src={assets.slider_1} alt="Item 1" />
+      <Carousel
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={3000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        deviceType="desktop"
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
+        <div className="p-4">
+          <img
+            src={assets.slider_1}
+            alt="Item 1"
+            className="w-full h-auto"
+          />
         </div>
-        <div className="item">
-          <img src={assets.slider_2} alt="Item 2" />
+        <div className="p-4">
+          <img
+            src={assets.slider_2}
+            alt="Item 2"
+            className="w-full h-auto"
+          />
         </div>
-        <div className="item">
-          <img src={assets.slider_3} alt="Item 3" />
+        <div className="p-4">
+          <img
+            src={assets.slider_3}
+            alt="Item 3"
+            className="w-full h-auto"
+          />
         </div>
-      </OwlCarousel>
+      </Carousel>
     </div>
   );
 };
