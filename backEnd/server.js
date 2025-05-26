3a37023cdbd49716e773c01b570c943f1b01dcd0
mohamedhamdy9966 <mh7008002@gmail.com>
2025-05-26 22:33:25 +0300

    fix

--- backEnd/server.js
+++ backEnd/server.js
@@ -27,6 +27,22 @@ app.use(
 
 // Parse JSON requests
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  next();
+});
 
 // API Routes
 app.use("/api/user", userRouter);
